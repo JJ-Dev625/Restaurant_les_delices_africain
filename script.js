1f600959532133cc227b9b8d3b2609657ef7e2aa
JJ-Dev625 <cloudu86@gmail.com>
2026-06-20 14:18:09 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // ===== SLIDESHOW HERO =====
+  const heroImages = [
+    "./assets/images/inté.jpg",
+    "./assets/images/int4.jpg",
+    "./assets/images/int.jpg"
+  ];
