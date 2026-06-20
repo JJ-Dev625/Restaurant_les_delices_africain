461d08f647dd770b0bc352e35c73fd8de3987209
JJ-Dev625 <cloudu86@gmail.com>
2026-06-20 15:02:38 +0100

    modification de l'ancre et du chemin

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   // ===== SLIDESHOW HERO =====
   const heroImages = [
-    "./assets/images/inté.jpg",
+    "./assets/images/int1.jpg",
     "./assets/images/int4.jpg",
     "./assets/images/int.jpg"
   ];
